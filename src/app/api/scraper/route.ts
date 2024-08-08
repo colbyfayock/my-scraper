f88f36fc0b56eef622437696626d6459f95154e0
Colby Fayock <fayock@gmail.com>
2024-08-07 22:26:30 -0400

    Bonus: Uploading a screenshot to Cloudinary

--- src/app/api/scraper/route.ts
+++ src/app/api/scraper/route.ts
@@ -1,5 +1,12 @@
 import chromium from '@sparticuz/chromium-min';
 import puppeteer from 'puppeteer-core';
+import { v2 as cloudinary } from 'cloudinary';
+
+cloudinary.config({
+  cloud_name: process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.NEXT_PUBLIC_CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
 export const maxDuration = 20;
 
@@ -18,10 +25,23 @@ export async function POST(request: Request) {
   const page = await browser.newPage();
   await page.goto(siteUrl);
   const pageTitle = await page.title();
+  const screenshot = await page.screenshot();
   await browser.close();
+
+  const resource = await new Promise((resolve, reject) => {
+    cloudinary.uploader.upload_stream({}, function (error: unknown, result: unknown) {
+      if (error) {
+        reject(error);
+        return;
+      }
+      resolve(result);
+    })
+    .end(screenshot);
+  });
 
   return Response.json({
     siteUrl,
     pageTitle,
+    resource
   })
 }
