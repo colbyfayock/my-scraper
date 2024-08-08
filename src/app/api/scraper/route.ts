6e878b862d73e357fc6ed7c0759a34c0491792b5
Colby Fayock <fayock@gmail.com>
2024-08-07 21:28:43 -0400

    Step 2: Setting up and configuring Puppeteer in a Next.js API route

--- src/app/api/scraper/route.ts
+++ src/app/api/scraper/route.ts
@@ -1,6 +1,25 @@
+import chromium from '@sparticuz/chromium-min';
+import puppeteer from 'puppeteer-core';
+
 export async function POST(request: Request) {
   const { siteUrl } = await request.json();
+
+  const isLocal = !!process.env.CHROME_EXECUTABLE_PATH;
+
+  const browser = await puppeteer.launch({
+    args: isLocal ? puppeteer.defaultArgs() : chromium.args,
+    defaultViewport: chromium.defaultViewport,
+    executablePath: process.env.CHROME_EXECUTABLE_PATH || await chromium.executablePath('https://<Bucket Name>.s3.amazonaws.com/chromium-v126.0.0-pack.tar'),
+    headless: chromium.headless,
+  });
+
+  const page = await browser.newPage();
+  await page.goto(siteUrl);
+  const pageTitle = await page.title();
+  await browser.close();
+
   return Response.json({
-    siteUrl
+    siteUrl,
+    pageTitle,
   })
 }
